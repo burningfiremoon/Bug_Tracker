2e97d76f090f63db71032b1b66fddd882215c46c
propigftw <66706834+propigftw@users.noreply.github.com>
2024-07-03 16:25:14 -0700

    Update control.h

--- control.h
+++ control.h
@@ -1,13 +1,34 @@
-// control.h
+/*
+    Revision History:
+    1.0 - 03-05-2024 [Date] - Modified by Skyler
+
+    control.h:
+    This header file contains function declarations for handling different scenarios.
+    It includes necessary headers and defines functions to handle various use cases.
+*/
+//-------------------------------------
 #ifndef CONTROL_H
 #define CONTROL_H
 
+//-------------------------------------
+// List of #includes
 #include "database.h"
 #include "print.h"
 #include "ui.h"
 
+//-------------------------------------
+// Function prototypes
+
 //functions taking care of different scenarios
+
+// -----
+// UC1
+// Handles scenario 1
 void UC1();
+
+// -----
+// UC2
+// Handles scenario 2
 void UC2();
 void UC3();
 void UC4();
