cdd09a87c4ab58bb7efa247a1c764bbb54b5d0a6
propigftw <66706834+propigftw@users.noreply.github.com>
2024-07-03 16:36:28 -0700

    Update print.h

--- print.h
+++ print.h
@@ -1,11 +1,30 @@
-// print.h
+/*
+    Revision History:
+    1.0 - 03-05-2024 [Date] - Modified by Anthony
+
+    print.h:
+    This header file contains function declarations for printing various reports.
+*/
+//-------------------------------------
 #ifndef PRINT_H
 #define PRINT_H
 
-// functions for printing report scenarios
-//  ***might need to add arguments for functions below
+//-------------------------------------
+// Function prototypes
+
+// -----
+// printChangeItem
+// Prints details of a change item
 void printChangeItem();
+
+// -----
+// printOpenBugs
+// Prints a list of open bugs
 void printOpenBugs();
+
+// -----
+// printStatusReport
+// Prints a status report
 void printStatusReport();
 
 #endif // PRINT_H
