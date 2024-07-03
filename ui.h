b40ec68950fc4e5f1b1cf9e426740797b092ecd8
propigftw <66706834+propigftw@users.noreply.github.com>
2024-07-03 16:36:52 -0700

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,12 +1,36 @@
-// ui.h
+/*
+    Revision History:
+    1.0 - 03-05-2024 [Date] - Modified by Tanvir
+
+    ui.h:
+    This header file contains function declarations for the user interface.
+    It includes the main menu and other UI functions.
+*/
+//-------------------------------------
 #ifndef UI_H
 #define UI_H
 
+//-------------------------------------
+// List of #includes
 #include "control.h"
 
-//main menu ui with switch case calling different functions from control.h
+//-------------------------------------
+// Function prototypes
+
+// -----
+// mainMenu
+// Displays the main menu and handles user input
+// returns: true if user chooses to exit, false otherwise (out)
 bool mainMenu();
+
+// -----
+// showUserInfo
+// Displays user information
 void showUserInfo();
+
+// -----
+// showChangeRequestReport
+// Displays a report for change requests
 void showChangeRequestReport();
 
 #endif // UI_H
