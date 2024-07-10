e26e1c0741a512512c6f313d62648c66f6defd63
burningfiremoon <charleszhang4@gmail.com>
2024-07-10 13:15:34 -0700

    Created basic functions for each menu, and Completed MainMenu()

--- ui.h
+++ ui.h
@@ -20,7 +20,10 @@ using namespace std;
 //-------------------------------------
 // Declairing functions
 int MainMenu();
-
+void CreateCustomerRequest();
+void ModifyRequest();
+int PrintReportsAndInquiries();
+void BackupData();
 
 //-------------------------------------
 // Function prototypes
@@ -30,26 +33,101 @@ int MainMenu();
 // Displays the main menu and handles user input
 // returns: true if user chooses to exit, false otherwise (out)
 bool TextUI(){
-    MainMenu();
+    switch(MainMenu()){
+        case 1:
+            CreateCustomerRequest();
+            break;
+        case 2:
+            ModifyRequest();
+            break;
+        case 3:
+            PrintReportsAndInquiries();
+            break;
+        case 4:
+            BackupData();
+            break;
+        case 0:
+            return true;
+        default:
+            // Error message
+            cout << "Substitute Error Message" << endl;
 
-    return true;
+    }
+
+    return false;
 };
 
 // -----
 // Main Menu
-// Prints Main Menu, waits for input, and returns
-// which menu is selected by User
+// Prints Main Menu, waits for input, and 
+// returns which menu is selected by User
 int MainMenu(){
+    int input;
     cout << "=== Main Menu ===" << endl;
     cout << "1) Create Customer Request" << endl;
-    cout << "2) Modify/Update Change/Feature Request" << endl;
+    cout << "2) Modify Change/Feature Request" << endl;
     cout << "3) Print Reports and Inquiries" << endl;
     cout << "4) Backup Data" << endl;
     cout << "0) Exit" << endl;
-
-    return 1;
+    cout << "Input your selection (0-4) and hit Enter: ";
+    cin >> input;
+    return input;
 }
 
+// -----
+// CreateCustomerRequest
+//
+void CreateCustomerRequest(){
+    int input;
+    cout << "=== Customer Request ===" << endl;
+    cout << "1) New Customer" << endl;
+    cout << "2) Existing Customer" << endl;
+    cout << "0) Return to Main Menu" << endl;
+    cout << "Input your selection (0-2) and hit Enter: ";
+    cin >> input;
+}
+
+// -----
+// Modify Change/Feature Request
+//
+void ModifyRequest(){
+    int input;
+    cout << "=== Modify Change/Feature Request ===" << endl;
+    cout << "1) Update status" << endl;
+    cout << "2) Update anticipated release date" << endl;
+    cout << "3) Update description" << endl;
+    cout << "0) Return to Main Menu" << endl;
+    cout << "Input your selection (0-3) and hit Enter: ";
+    cin >> input;
+}
+
+// -----
+// PrintReportsAndInquiries
+// Prints Menu for print reports and inquiries and
+// Returns which action is selected by user
+int PrintReportsAndInquiries(){
+    int input;
+    cout << "=== Print Reports and Inquiries ===" << endl;
+    cout << "1) Print Report for managers" << endl;
+    cout << "2) Print Report for customer inquiries" << endl;
+    cout << "0) Return to Main Menu" << endl;
+    cout << "Input your selection (0-2) and hit Enter: ";
+    cin >> input;
+    return input;
+}
+
+// -----
+// BackupData
+//
+void BackupData(){
+    int input;
+    cout << "=== Backup Data ===" << endl;
+    cout << "1) Backup the entire database" << endl;
+    cout << "2) Backup specific data sets" << endl;
+    cout << "0) Return to Main Menu" << endl;
+    cout << "Input your selection (0-2) and hit Enter: ";
+    cin >> input;
+}
 
 
 // -----
