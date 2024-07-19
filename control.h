cc85e3c7f49aef2e3180e790d2ef246b8e0e387f
burningfiremoon <charleszhang4@gmail.com>
2024-07-18 19:25:28 -0700

    Added comments

--- control.h
+++ control.h
@@ -22,25 +22,72 @@
 
 //functions taking care of different scenarios
 
-
-
+// -----
+// StopAccepting
 void StopAccepting();
+
+// -----
+// StartAccepting
 void StartAccepting();
+
+// -----
+// CloseDatabase
 void CloseDatabase();
+
+// -----
+// OpenDatabase
 void OpenDatabase();
 
-
-void UpdateUserName(char Name[31],char newName[31]);
-void UpdateUserEmail(char Name[31], char newEmail[25]);
-void UpdateUserPhoneNumber(char Name[31], char newPhoneNumber[18]);
-void UpdateChangeItemStatus(int changeID, int status);
-bool CheckUserExists(char Name[31]);
-bool CheckChangeItemExists(int ChangeID);
-bool CheckProductExists(char Product[11]);
-bool CheckProductReleaseExists(char Productname[11]);
 // -----
-// Check Status report
-// receive date range in the format (YYYY-MM-DD to YYYY-MM-DD) and couts all status reports in date range
+// UpdateUserName
+// char[31] Name (in)
+// char[31] newName (in)
+void UpdateUserName(char Name[31], char newName[31]);
+
+// -----
+// UpdateUserEmail
+// char[31] Name (in)
+// char[25] newEmail (in)
+void UpdateUserEmail(char Name[31], char newEmail[25]);
+
+// -----
+// UpdateUserPhoneNumber
+// char[31] Name (in)
+// char[18] newPhoneNumber (in)
+void UpdateUserPhoneNumber(char Name[31], char newPhoneNumber[18]);
+
+// -----
+// UpdateChangeItemStatus
+// int changeID (in)
+// int status (in)
+void UpdateChangeItemStatus(int changeID, int status);
+
+// -----
+// CheckUserExists
+// char[31] Name (in)
+// bool value if object exists (out)
+bool CheckUserExists(char Name[31]);
+
+// -----
+// CheckChangeItemExists
+// int ChangeID (in)
+// bool value if object exists (out)
+bool CheckChangeItemExists(int ChangeID);
+
+// -----
+// CheckProductExists
+// char[11] Product (in)
+// bool value if object exists (out)
+bool CheckProductExists(char Product[11]);
+
+// -----
+// CheckProductReleaseExists
+// char[11] Productname (in)
+// bool value if object exists (out)
+bool CheckProductReleaseExists(char Product[11]);
+
+// -----
+// ViewStatusReport
 // Date start (in)
 // Date end (in)
 // Open Change Items between date range (out)
@@ -48,73 +95,128 @@ void ViewStatusReport(Date start, Date end);
 
 // -----
 // showUserInfo
-// Displays user information
+// char[31] Name (in)
 void showUserInfo(char Name[31]);
 
 // -----
-// showChangeRequestReport
-// Displays a report for change requests
+// showChangeItemReport
+// int changeID (in)
 void showChangeItemReport(int changeID);
 
-// Show Change Items
-// Displays All Change items for ProductID
+// -----
+// ShowChangeItems
+// char[31] ProductID (in)
 void ShowChangeItems(char ProductID[31]);
 
 // -----
-// Connect Change Request
-// Connects matching change request to change item
+// connectChangeRequest
+// char[31] Name (in)
+// int changeID (in)
 void connectChangeRequest(char Name[31], int changeID);
 
+// -----
+// ViewChangeItem
+// int changeID (in)
 void ViewChangeItem(int changeID){
     // Check existence
     CheckChangeItemExists(changeID);
 }
-void PrintOpenBugs(char Product[11],Date start, Date end);
+
+// -----
+// PrintOpenBugs
+// char[11] Product (in)
+// Date start (in)
+// Date end (in)
+void PrintOpenBugs(char Product[11], Date start, Date end);
+
+// -----
+// Init_ProductRelease
+// char[11] Product (in)
+// Date date (in)
+// bool value if initialization was successful (out)
 bool Init_ProductRelease(char Product[11], Date date);
 
 // -----
 // Init_ChangeItem
-// creates a new change Item object using Product name and a change description and outputs a new unique changeID
+// char[11] Product (in)
+// char[31] ChangeDescription (in)
+// returns new unique (int) changeID (out)
 int Init_ChangeItem(char Product[11], char ChangeDescription[31]);
 
-
+// -----
+// Init_User
+// char[31] Name (in)
+// char[18] PhoneNumber (in)
+// char[25] Email (in)
+// bool value if initialization was successful (out)
 bool Init_User(char Name[31], char PhoneNumber[18], char Email[25]);
-bool Init_ChangeRequest(char Name[31], char ProductName[31], char ReleaseID[9], char Description[31], int Priority);
 
-bool Init_Product();
+// -----
+// Init_ChangeRequest
+// char[31] Name (in)
+// char[11] Product (in)
+// char[9] ReleaseID (in)
+// char[31] Description (in)
+// int Priority (in)
+// bool value if initialization was successful (out)
+bool Init_ChangeRequest(char Name[31], char Product[11], char ReleaseID[9], char Description[31], int Priority);
+
+// -----
+// Init_Product
+// char[11] Product (in)
+// bool value if initialization was successful (out)
+bool Init_Product(char Product[11]);
+
+// -----
+// PrintChangeItem
+// int ChangeID (in)
 void PrintChangeItem(int ChangeID);
-bool PrintUserInfo(char Name[31]);
+
+// -----
+// PrintUserInfo
+// char[31] Name (in)
+void PrintUserInfo(char Name[31]);
+
+// -----
+// UpdateChangeItemReleaseDate
+// int changeID (in)
+// Date date (in)
 void UpdateChangeItemReleaseDate(int changeID, Date date);
+
+// -----
+// UpdateChangeItemDescription
+// int changeID (in)
+// char[31] Description (in)
 void UpdateChangeItemDescription(int changeID, char Description[31]);
 
 // -----
 // BackUpChangeRequest
-// Backs all ChangeRequests in database into a new file
+// bool value if back up was successful (out)
 bool BackUpChangeRequest();
 
 // -----
 // BackUpUsers
-// Backs all Users in database into a new file
+// bool value if back up was successful (out)
 bool BackUpUsers();
 
 // -----
 // BackUpChangeItems
-// Backs all Change Items in database into a new file
+// bool value if back up was successful (out)
 bool BackUpChangeItems();
 
 // -----
 // BackUpProductReleases
-// Backs all Product Releases in database into a new file
+// bool value if back up was successful (out)
 bool BackUpProductReleases();
 
 // -----
 // BackUpProducts
-// Backs all Products in database into a new file
+// bool value if back up was successful (out)
 bool BackUpProducts();
 
 // -----
 // BackUpEntireDatabase
-// Backs all files in database into a new file
+// bool value if back up was successful (out)
 bool BackUpEntireDatabase(){
     BackUpChangeRequest();
     BackUpUsers();
@@ -122,5 +224,6 @@ bool BackUpEntireDatabase(){
     BackUpProductReleases();
     BackUpProducts();
 }
+
 
 #endif // CONTROL_H
